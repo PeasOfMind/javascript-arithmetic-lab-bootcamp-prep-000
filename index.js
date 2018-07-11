c1a1f6732b6691321ee0a7f7261064b59fef9077
Yeqing Liu <sunsetpeak42@gmail.com>
2018-07-11 00:02:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,4 +10,16 @@ function multiply(a,b){
   return a*b;
 }
 
-function
+function divide(a,b){
+  return a/b;
+}
+
+function inc(n){
+  // return n += 1;
+  return n++
+}
+
+function dec(n){
+  // return n -= 1;
+  return n--
+}
